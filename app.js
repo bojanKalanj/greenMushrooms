f55c2a5ee5151f3e428c32f257a4a19a5259908e
bojanKalanj <bojan.kalanj@gmail.com>
2018-01-07 19:57:53 +0100

    searh

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ document.querySelector('.menu').addEventListener('click', function(){
     document.querySelector('.menu-dropdown').classList.toggle('menu-dropdown-active');
 })
 
+document.querySelector('.search .fa-search').addEventListener('click', function(){
+    document.querySelector('.search-dropdown').classList.toggle('search-dropdown-active');
+})
+
 window.addEventListener('scroll', function(){
     var scroll = window.pageYOffset;
     if(scroll > 100){
